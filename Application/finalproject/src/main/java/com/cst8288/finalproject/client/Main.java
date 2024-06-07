adf8cc8182c6e16730d768cd636a2debeb8ccdf8
jgarw <user@JGMB.local>
2024-06-07 16:55:35 -0400

    created maven project, and set up blank classes for Main, User, DAO, and DTO

---
+++ Application/finalproject/src/main/java/com/cst8288/finalproject/client/Main.java
@@ -0,0 +1,12 @@
+package com.cst8288.finalproject.client;
+
+/**
+ * This class will act as the driver class that will execute the program.
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
