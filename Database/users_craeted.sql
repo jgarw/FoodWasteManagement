e096ec6b2fae1b5996fcaf9fc6718ab503ea4513
jgarw <garw0004@algonquinlive.com>
2024-06-09 11:23:59 -0400

    created a quick sql script for users tables

---
+++ Database/users_craeted.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE FoodWasteReduction;
+
+USE FoodWasteReduction;
+
+CREATE TABLE USERS(
+user_id int AUTO_INCREMENT PRIMARY KEY,
+name varchar(100) NOT NULL,
+email varchar(100) NOT NULL UNIQUE,
+password varchar(100) NOT NULL,
+user_type varchar(50) NOT NULL,
+phone varchar(50)
+);
+
+SELECT * FROM USERS;
+
+CREATE TABLE CONSUMERS(
+consumer_id int AUTO_INCREMENT PRIMARY KEY,
+user_id int NOT NULL,
+FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
+
+SELECT * FROM CONSUMERS;
+
+CREATE TABLE RETAILERS(
+retailer_id int AUTO_INCREMENT PRIMARY KEY,
+user_id int NOT NULL,
+FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
+
+SELECT * FROM RETAILERS;
+
+CREATE TABLE ORGANIZATIONS(
+org_id int AUTO_INCREMENT PRIMARY KEY,
+user_id int NOT NULL,
+FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
+
+SELECT * FROM ORGANIZATIONS;
